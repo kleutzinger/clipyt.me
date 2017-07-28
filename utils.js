37f72d0c8f7843850622a4457a4ce6534e5dd3d9
Kevin Leutzinger <me@kevinleutzinger.com>
2017-07-27 23:19:21 -0700

    changed to clipyt.me

--- utils.js
+++ utils.js
@@ -115,7 +115,7 @@ function constructHash(){
 function sendButton(){
 	v=videos;
     hash_string = constructHash();
-    sendUrl = "http://www.kevinleutzinger.com/ytpartition/#" + hash_string;
+    sendUrl = "http://clipyt.me/#" + hash_string;
     //if (window.location.href.indexOf("local") != -1) {sendUrl = "http://localhost:8000/#" + hash_string;}
     window.prompt("Copy to clipboard: Ctrl+C (Cmd + C on mac)", sendUrl);
 }
