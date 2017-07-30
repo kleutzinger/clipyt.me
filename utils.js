a9e0b2e217a07d49a279da4d45cade64975e242f
Kevin Leutzinger <me@kevinleutzinger.com>
2017-07-29 19:50:30 -0700

    changed some css

--- utils.js
+++ utils.js
@@ -45,13 +45,13 @@ function writePartitions(p,boldIndex){
 		if (i==boldIndex){element+= "<strong>";}
 		element+=
 			'<li>'+ //
-			'<button id="vidli'+i+'" style="cursor:pointer;" onclick="playSingleClip('+i+')">&#9658;</button>&nbsp;' +
+			'<button id="vidli'+i+'" style="cursor:pointer;" onclick="playSingleClip('+i+')" class="control">&#9658;</button>&nbsp;' +
 			'<input id=start'+i+' oninput="startChange('+i+');" type="number" pattern=[0-9] value='+p[i].startSeconds+'>-'+
 			'<input id=end'  +i+' oninput="endChange  ('+i+');" type="number" pattern=[0-9] value='+p[i].endSeconds+'>&nbsp;&nbsp;'+
-			'<button onclick="moveUp('+i+')">  &#8593;</button>'+
-			'<button onclick="moveDown('+i+')">&#8595;</button>&nbsp;'+
-			'<button onclick="deleteClip('+i+');"> &#215;</button>&nbsp;&nbsp;'+
-			'<a id="vidlink'+i+'" href="https://youtu.be/'+p[i].vid+'?t='+p[i].startSeconds+'">'+p[i].name+'</a>' +
+			'<button onclick="moveUp('+i+')"      class="control">  &#8593;</button>'+
+			'<button onclick="moveDown('+i+')"    class="control">&#8595;</button>&nbsp;'+
+			'<button onclick="deleteClip('+i+');" class="control"> &#215;</button>&nbsp;&nbsp;'+
+			'<a id="vidlink'+i+'" href="https://youtu.be/'+p[i].vid+'?t='+p[i].startSeconds+'" class="vidlink">'+p[i].name+'</a>' +
 			'</li>';
 		if(i==boldIndex){element+="</strong>";}
 		$("#partition_list").append($(element));
@@ -64,13 +64,12 @@ function writePartitions(p,boldIndex){
 
 function bolden(i){
 	for(n=0;n<videos.length;n++){
-		$("#vidli"+n).css('color', 'black');
+		$("#vidli"+n).css('background-color', 'white');
 		if(n==i){
-			$("#vidli"+n).css('color', 'red');
+			$("#vidli"+n).css('background-color', 'red');
 		}
 	}
 }
-
 
 function constructHash(){
 	setIndexButton(videos.length);
